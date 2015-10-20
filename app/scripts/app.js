e097802f7e19d77cbe01b85fb30099ea860cab09
efrengarcial <efren.gl@gmail.com>
2015-10-19 22:53:17 -0500

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name portalWebApp
+ * @description
+ * # portalWebApp
+ *
+ * Main module of the application.
+ */
+angular
+    .module('portalWebApp', [
+        'ngRoute',
+        'ngSanitize'
+    ])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .when('/login', {
+                templateUrl: 'views/login.html',
+                controller: 'AccountController'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
---
+++ app/scripts/controllers/accountcontroller.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name portalWebApp.controller:AccountController
+ * @description
+ * # AccountController
+ * Controller of the portalWebApp
+ */
+angular.module('portalWebApp')
+  .controller('AccountController', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
